1e8ca8c28ad2c7abf9be9fe475f1c25376b79eb8
เอกการัณภูมิ ตันเยี่ยนนิติ <thanyalak15601@gmail.com>
2023-11-21 12:04:09 +0700

    built Navbar with React Bootstrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div className='content'>
+      </div>
+    </div>
+  );
+}
+
+export default App;
